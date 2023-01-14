9278b0a8c45675bb70111a04be7e9a7a600e7a26
aBorovina1974 <aborovina74@gmail.com>
2023-01-14 22:13:43 +0100

    Using built in Fragment component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
 import AddUser from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
 
@@ -17,12 +17,12 @@ function App() {
   };
 
   return (
-    <div>
+    <Fragment>
       <AddUser onAddUser={addToUsersHandler} />
       {users.length > 0 && (
         <UsersList users={users} onDeleteUser={deleteFromUsersHandler} />
       )}
-    </div>
+    </Fragment>
   );
 }
 
