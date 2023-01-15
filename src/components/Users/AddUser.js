549ba97d64150b865cebb1f9b9c0edd303e2da0b
aBorovina1974 <aborovina74@gmail.com>
2023-01-15 21:32:41 +0100

    Using Ref

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -10,6 +10,7 @@ function AddUser(props) {
 
   const addUserHandler = (event) => {
     event.preventDefault();
+
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
       setError({
         title: "Invalid input",
@@ -80,3 +81,75 @@ function AddUser(props) {
   );
 }
 export default AddUser;
+
+//Alternative syntax using Refs
+/* import React, { useState, useRef, Fragment } from "react";
+import Card from "../UI/Card";
+import Button from "../UI/Button";
+import styles from "./AddUser.module.css";
+import ErrorModal from "../UI/ErrorModal";
+function AddUser(props) {
+  const [error, setError] = useState();
+
+  const usernameInputRef = useRef();
+  const ageInputRef = useRef();
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+
+    const enteredUsername = usernameInputRef.current.value;
+    const enteredAge = ageInputRef.current.value;
+
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: "Invalid input",
+        message:
+          "Both fields are required! Please enter valid username and age.",
+      });
+      return;
+    }
+    if (+enteredAge < 1) {
+      setError({
+        title: "Invalid input",
+        message: "Please enter a valid age value (> 0).",
+      });
+      return;
+    }
+
+    props.onAddUser({
+      name: enteredUsername,
+      age: enteredAge,
+      id: Math.random().toString(),
+    });
+
+    //Manipulating the DOM without React(Not recommended!)
+    usernameInputRef.current.value = "";
+    ageInputRef.current.value = "";
+  };
+
+  const errorHandler = () => {
+    setError(null);
+  };
+
+  return (
+    <Fragment>
+      {error && (
+        <ErrorModal
+          onError={errorHandler}
+          title={error.title}
+          message={error.message}
+        />
+      )}
+      <Card className={styles["form-card"]}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input id="username" type="text" ref={usernameInputRef} />
+          <label htmlFor="age">Age (Years)</label>
+          <input id="age" type="number" ref={ageInputRef} />
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </Fragment>
+  );
+}
+export default AddUser; */
